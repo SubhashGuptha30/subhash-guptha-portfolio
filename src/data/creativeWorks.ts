0d47335fcf06c91d87df2c617d897858ede1ddaa
SubhashGuptha30 <subbuguptha2005@outlook.com>
2025-06-12 22:21:26 +0530

    modified creative works

--- src/data/creativeWorks.ts
+++ src/data/creativeWorks.ts
@@ -4,8 +4,8 @@ export const creativeWorks = [
     title: "Vijayanath - A Warrior's Tale",
     description: "An epic tale of courage and honor set in ancient times, exploring themes of duty and sacrifice.",
     type: "Script",
-    image: "https://i.postimg.cc/3x5f1j6H/image.png",
-    link: null
+    image: "/uploads/vijayanath.jpg",
+    link: "https://docs.google.com/document/d/1OOodgSuOLy_qk2aNxi7Zj4rZyP6UUpA-E4F91JYyqyM/edit?usp=sharing",
   },
   {
     title: "Reflections in Code",
